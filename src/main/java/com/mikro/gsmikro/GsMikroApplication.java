4756ad536cd2c3dfb1739324178d6c8b00b7e3ca
alvinowyong <70066269+alvinowyong@users.noreply.github.com>
2021-08-27 20:06:10 +0800

    Initial commit
    Spring setup files

---
+++ src/main/java/com/mikro/gsmikro/GsMikroApplication.java
@@ -0,0 +1,13 @@
+package com.mikro.gsmikro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsMikroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsMikroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikro/gsmikro/GsMikroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikro.gsmikro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsMikroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
